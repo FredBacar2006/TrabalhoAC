96870985cd71cf6b5bf0c9311e1c37cfdead6c30
fredericobacar <fredericobacar2006@gmail.com>
2023-10-12 10:45:08 +0100

    Trabalho AC, Fred, Helio , Bohdan

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabalhoAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
